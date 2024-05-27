daa4b2e1b4f45743d2c949572216600eeb57eff3
FabioCunha105089 <a105089@alunos.uminho.pt>
2024-05-27 22:55:05 +0100

    erwq

--- data/xmls_to_json.py
+++ data/xmls_to_json.py
@@ -1,13 +1,11 @@
 import xml.etree.ElementTree as ET
 import json
-
+# Ruas
 with open('ruas.xml', 'r') as xml_file:
     xml_ruas = xml_file.read()
     
-# Parse the XML
 root = ET.fromstring(xml_ruas)
 
-# Transform the XML to the desired JSON structure
 ruas_list = []
 for rua in root.findall('rua'):
     nome = rua.find('nome').text
@@ -17,9 +15,29 @@ for rua in root.findall('rua'):
       'nome': nome
     })
 
-# Convert to JSON
-json_output = json.dumps(ruas_list, indent=2, ensure_ascii=False)
-
-# Write to a JSON file
 with open('ruas.json', 'w') as json_file:
-    json_file.write(json_output)
+    json_file.write(json.dumps(ruas_list, indent=2, ensure_ascii=False))
+    
+
+with open('lugares.xml', 'r') as xml_file:
+    xml_lugares = xml_file.read()
+
+
+root = ET.fromstring(xml_lugares)
+
+lugares_list = []
+for i, lugar in enumerate(root.findall('lugar'), start=1):
+    _id = str(i)
+    nome = lugar.find('nome').text
+    rua = lugar.find('rua').text
+    lugares_list.append({
+        '_id': _id,
+        'nome': nome,
+        'rua': rua
+    })
+
+with open('lugares.json', 'w') as json_file:
+    json_file.write(json.dumps(lugares_list, indent=2, ensure_ascii=False))
+
+
+# TODO Entidades para json. Agrupar por tipo
